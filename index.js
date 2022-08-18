26d2a7fd7992b79c99ed925e5715cc27c0b193a2
ar <a.rau@comlog.org>
2022-08-18 12:29:28 +0200

    ...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var util = require('util');
-Sipgate = require('./sipgate');
+var Sipgate = require('sipgateio');
 
 function SipgateSMS(options) {
 	var _this = this;
@@ -9,8 +9,8 @@ function SipgateSMS(options) {
 	this.timeout = 5000;
 
 	this.text = "Service {$label} status changed to {$status} at {$datetime}";
-	this.to = "target-number"; // 49123456789
-	this.from = 'sipgate-number'; // 49987654321
+	this.to = "target-number"; // 0123456789
+	this.from = 'sipgate-number'; // 0987654321
 	this.debug = false;
 	this.label = null;
 
@@ -22,61 +22,74 @@ function SipgateSMS(options) {
 			err = new Error(reason);
 			if (_this.debug) _this.logger.error(err);
 		} else {
-			if (_this.debug) _this.logger.debug(util.inspect(res, {
-				colors: true,
-				depth: null
-			}));
+			_this.logger.debug('SMS Sent!');
 		}
 		if (cb) cb(err, res);
 	};
 
 	this.start = function(info, cb) {
 		var opt = {
-			text: this.text,
+			message: this.text,
 			to: this.to,
-			from: this.from
+			smsId: 's1'
 		};
+
+		if (this.from) opt.from = this.from;
 
 		if (!info) info = {};
 		info.datetime = (new Date())+'';
 		for(var i in info) {
 			if (typeof info[i] !== 'string' && typeof info[i] !== 'number') continue;
-			opt.text = opt.text.split('{$'+i+'}').join(info[i]);
+			opt.message = opt.message.split('{$'+i+'}').join(info[i]);
 			opt.to = opt.to.split('{$'+i+'}').join(info[i]);
 		}
 
 		for(var i in this) {
 			if (typeof this[i] !== 'string' && typeof this[i] !== 'number') continue;
-			opt.text = opt.text.split('{$'+i+'}').join(this[i]);
+			opt.message = opt.message.split('{$'+i+'}').join(this[i]);
 			opt.to = opt.to.split('{$'+i+'}').join(this[i]);
 		}
 
-		/*new Sipgate({"user": _this.user, "pass": _this.password}, function(sipgate) {
-			return sipgate.ownUriListGet().then(
-				function (res) { return _handle(null, res, cb); }
-				, function (res) { return _handle(res, null, cb); }
-			);
-		});*/
-		new Sipgate({"user": _this.user, "pass": _this.password}, function(sipgate) {
-			return sipgate.sessionInitiate({
-				TOS:'text',
-				RemoteUri: 'sip:'+opt.to+'@sipgate.net',
-				LocalUri: 'sip:'+opt.from+'@sipgate.net',
-				Content: opt.text
-			}).then(
-				function (res) { _handle(null, res, cb); },
-				function (res) { _handle(res, null, cb); }
-			);
-		});
+		var client;
+		if (_this.user.indexOf('token') === 0) {
+			client = Sipgate.sipgateIO({tokenId: _this.user, token: _this.password});
+		}
+		else {
+			client = Sipgate.sipgateIO({username: _this.user, password: _this.password});
+		}
+
+		var sms = Sipgate.createSMSModule(client);
+
+		sms
+			.send(opt)
+			.then(function () {
+				debugger;
+				_handle(null, null, cb);
+				console.log('Sms sent.');
+			})
+			.catch(function (e) {
+				debugger;
+				_handle(e, null, cb);
+			});
+
+		return;
 	};
 
 	if (options) for(var i in options) this[i] = options[i];
 	if (!this.label) this.label = this.name || this.host || 'unbekannt';
 
-	this.to = this.to.replace(/[^0-9]/g, '');
-	if (this.to.substr(0, 2) === '00') this.to = this.to.substr(2);
-	if (this.to.substr(0, 1) === '0') this.to = '49' + this.to.substr(1);
-	if (this.to.substr(0, 1) === '+') this.to = this.to.substr(1);
+	var escape = function (nr) {
+		if (!nr) return nr;
+		nr = nr.replace(/[^0-9]/g, '');
+		if (nr.substring(0, 2) === '00') nr = nr.substring(2);
+		if (nr.substring(0, 1) === '0') nr = '49' + nr.substring(1);
+		if (nr.substring(0, 1) === '+') nr = nr.substring(1);
+		nr = '+'+nr;
+		return nr;
+	}
+
+	this.to = escape(this.to);
+	this.from = escape(this.from);
 }
 
 module.exports = SipgateSMS;
